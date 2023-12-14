4ed6c9ff5ae488c2c3fff4b05bbcd5070b7b1b66
Lukas Mertens <144951668+Lukasmrt00@users.noreply.github.com>
2023-12-14 12:31:30 +0100

    Update test_packages.R

--- test_packages.R
+++ test_packages.R
@@ -1,8 +1,4 @@
 library(shiny)
 library(DBI)
 library(RPostgres)
-<<<<<<< HEAD
-library(ggplot2) # This will not work
-=======
 library(ggplot2) # test 123
->>>>>>> 91d1d85c696e7a56994af6d77b790c8e3052ebab
